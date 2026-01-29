33c7e054749f4cc210d0b04dc4e5c5fabe380efc
sazzad <sazzadador@gmail.com>
2026-01-29 18:59:34 +0600

    implement prev image navigation logic

--- slider.js
+++ slider.js
@@ -9,7 +9,6 @@ const sliderImage = document.getElementById("sliderImage");
 let currentIndex = 0;
 function nextImage() {
   currentIndex++;
-  
 
   if (currentIndex >= images.length) {
     currentIndex = 0;
@@ -18,4 +17,13 @@ function nextImage() {
     sliderImage.src = images[currentIndex];
   }
 }
-function prevImage() {}
+function prevImage() {
+  currentIndex--;
+
+  if (currentIndex < 0) {
+    currentIndex = images.length - 1;
+    sliderImage.src = images[currentIndex];
+  } else {
+    sliderImage.src = images[currentIndex];
+  }
+}
