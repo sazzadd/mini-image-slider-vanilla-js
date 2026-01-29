ef28773d1ee9680241f8e9cdca527f59584d1959
sazzad <sazzadador@gmail.com>
2026-01-29 10:22:41 +0600

    nextImage() with condition

--- slider.js
+++ slider.js
@@ -1,8 +1,20 @@
 console.log("hello world");
- let images = ["img-1.jpg", "img1.jpg", "img2.jpg", "img3.jpg"];
-const sliderImage  = document.getElementById("sliderImage")
+const images = [
+  "img/img-1.jpg",
+  "img/img1.jpg",
+  "img/img2.jpg",
+  "img/img3.jpg",
+];
+const sliderImage = document.getElementById("sliderImage");
 let currentIndex = 0;
 function nextImage() {
- 
+  currentIndex++;
+
+  if (currentIndex >= images.length) {
+    currentIndex = 0;
+    sliderImage.src = images[currentIndex];
+  } else {
+    sliderImage.src = images[currentIndex];
+  }
 }
 function prevImage() {}
